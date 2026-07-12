bbec2c78673448b1212e73307f538925cfe885fe
Tamim379ta <tamimthasan55@gmail.com>
2026-07-13 02:25:13 +0600

    final push

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,7 +3,7 @@ import { inferAdditionalFields } from "better-auth/client/plugins";
 import type { auth } from "./auth";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
   plugins: [inferAdditionalFields<typeof auth>()],
 });
 
